8686176c98a092d4f8a8411fc3396a2ff0734b5f
Desisacc <saccescuela@gmail.com>
2024-09-23 23:02:16 -0600

    Sending token with user info and not in a cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,6 +28,7 @@ export const register = async (req, res) => {
             email: userSaved.email,
             createdAt: userSaved.createdAt,
             updatedAt: userSaved.updatedAt,
+            token: token,
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -53,6 +54,7 @@ export const login = async (req, res) => {
             email: userFound.email,
             createdAt: userFound.createdAt,
             updatedAt: userFound.updatedAt,
+            token: token,
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
