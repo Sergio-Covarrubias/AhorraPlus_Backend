ecead7133a375c4d4e82436b46c9721d54d878cd
Desisacc <saccescuela@gmail.com>
2024-09-23 20:25:47 -0600

    Update validateToken.js

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
 export const authRequired = (req, res, next) => {
-    console.log(req.cookies);
+    console.log('req.cookies:', req.cookies);
+    console.log('req.headers.cookie:', req.headers.cookie)
     const { token } = req.cookies;
     
     if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
